a45db2c56c860e2fcec32e9f94660eeda168a949
bessam <bsahli@wissal-sys.com>
2020-03-15 14:23:18 +0100

    Initial init

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {DashboardComponent} from './modules/dashboard/dashboard.component';
+import {DefaultComponent} from './layouts/default/default.component';
+import {PostsComponent} from './modules/posts/posts.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: DefaultComponent,
+    children: [{
+      path: '',
+      component: DashboardComponent
+    },
+      {
+        path: 'posts',
+        component: PostsComponent
+      }]
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ],
+  declarations: []
+})
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {DefaultModule} from './layouts/default/default.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    DefaultModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
